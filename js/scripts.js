dde3386be3f0360247e2202ad3f2108dbf3bb64c
Mgbanwune Onyekachi <standbymii@yahoo.com>
2021-10-29 14:59:57 +0100

    calculator with javascript

---
+++ js/scripts.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+    $(".addition").click(function(event) {
+        let input1 = ($("#firstnumber").val());
+        let input2 = ($("#secondnumber").val());
+        let  result = parseInt(input1) + parseInt(input2);
+        event.preventDefault()
+       //alert(result)
+       $("#result").text(result);
+       
+      });
+
+      $(".subtract").click(function(event) {
+        let input1 = ($("#firstnumber").val());
+        let input2 = ($("#secondnumber").val());
+        let  result = parseInt(input1) - parseInt(input2);
+        event.preventDefault()
+       alert(result)
+       $("#result").text(result);
+       
+      });
+
+      $(".multiply").click(function(event) {
+        let input1 = ($("#firstnumber").val());
+        let input2 = ($("#secondnumber").val());
+        let  result = parseInt(input1) * parseInt(input2);
+        event.preventDefault()
+       alert(result)
+       $("#result").text(result);
+       
+      });
+
+      $(".divide").click(function(event) {
+        let input1 = ($("#firstnumber").val());
+        let input2 = ($("#secondnumber").val());
+        let  result = parseInt(input1) / parseInt(input2);
+        event.preventDefault()
+       alert(result)
+       $("#result").text(result);
+       
+      });
+
+    })
